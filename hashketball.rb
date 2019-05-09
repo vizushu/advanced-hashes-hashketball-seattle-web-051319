f5fa87e32ae3c058e19e8e309d7abb0fdadc840d
beneficial-abstraction-6109 <me@renierdelacruz.com>
2019-05-09 21:28:11 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -147,9 +147,9 @@ def find_the_player(name)
   players.fetch(name)
 end
 
-def big_shoe_rebounds
-    all_players.sort_by { |player| player[:shoe] }[-1][:rebounds]
-end 
+# def big_shoe_rebounds
+#     all_players.sort_by { |player| player[:shoe] }[-1][:rebounds]
+# end 
 
 
 
